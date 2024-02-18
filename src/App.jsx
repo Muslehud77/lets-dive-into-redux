c022a4d525b34060a9b7da7ab4f08aad3cf75776
Muslehud77 <fardinmohit@gmail.com>
2024-02-19 03:47:18 +0800

    wrapped redux to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Home from "./Pages/Home"
 
 
 function App() {
@@ -5,7 +6,7 @@ function App() {
 
   return (
     <>
-    <h1 className="text-center text-5xl">Redux</h1>
+   <Home/>
     </>
   )
 }
