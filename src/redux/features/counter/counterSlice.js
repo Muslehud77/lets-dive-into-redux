450572ce7e2ae37e98ef9c5981617112972bd530
Muslehud77 <fardinmohit@gmail.com>
2024-02-19 05:40:31 +0800

    adding  the counter actions

--- src/redux/features/counter/counterSlice.js
+++ src/redux/features/counter/counterSlice.js
@@ -5,9 +5,19 @@ const initialState = {
 }
 
 const counterSlice = createSlice({
-    name : 'count',
+    name : 'counter',
     initialState,
-    reducers : {}
+    reducers : {
+        increment: (state)=>{
+            state.count = state.count + 1;
+        },
+        decrement: (state)=>{
+            state.count = state.count - 1;
+        },
+    }
 })
+
+export const {increment, decrement} = counterSlice.actions
+
 
 export default counterSlice.reducer
